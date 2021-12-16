2c5aa0f9f18a61561dc6dca5ccc453142dcfeb28
123456 <729745849@qq.com>
2021-12-16 22:25:59 +0800

    上传1

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-tabs": "off",
+	'no-multiple-empty-lines': 0,
+	"no-unused-vars": "off",
+	"no-irregular-whitespace": "off"
+  }
+}
